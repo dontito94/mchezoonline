67e3b4e3370eef36914b852961744d9b2b0d9e29
francistito <francistito07@gmail.com>
2020-08-20 14:44:06 +0300

    configuration and organization

---
+++ resources/views/includes/components/header.blade.php
@@ -0,0 +1,306 @@
+<header class="kode_header_2">
+    <!--Header 2 Top Bar Start-->
+    <div class="kf_top_bar">
+        <div class="container">
+            <div class="pull-left">
+                <ul class="kf_social2">
+                    <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                    <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                    <li><a href="#"><i class="fa fa-rss"></i></a></li>
+                </ul>
+            </div>
+            <div class="kf_right_dec">
+                <ul class="kf_topdec">
+                    <li>
+                        <div class="kf_lung">
+                            <span>Curreny :</span>
+                            <div class="dropdown">
+                                <button id="dLabel" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> USD
+                                </button>
+                                <ul class="dropdown-menu" aria-labelledby="dLabel">
+                                    <li>BRL</li>
+                                    <li>EUR</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </li>
+                    <li><a href="#">Support</a></li>
+                    <li><a href="#">Whishlist</a></li>
+                </ul>
+                <ul class="kf_user">
+                    <li><a href="#"><i class="fa fa-lock"></i>Sign up</a></li>
+                    <li><a href="#">Login</a></li>
+                </ul>
+                <a href="#" data-toggle="modal" data-target="#search" class="kode_search"><i class="fa fa-search"></i></a>
+            </div>
+        </div>
+    </div>
+    <!--Header 2 Top Bar End-->
+    <div class="container">
+        <!--Logo Bar Start-->
+        <div class="kode_logo_bar">
+            <!--Logo Start-->
+            <div class="logo">
+                <a href="#">
+                    <img src="images/logo 1.png" alt="">
+                </a>
+            </div>
+            <!--Logo Start-->
+            <!--Navigation Wrap Start-->
+            <div class="kode_navigation">
+                <!--Navigation Start-->
+                <ul class="nav">
+                    <li><a href="index-2.html">home</a></li>
+                    <li>
+                        <a href="#">Fixtures & Results</a>
+                        <ul>
+                            <li><a href="latest-result.html">latest result</a></li>
+                            <li><a href="team-schedule.html">teamschedule</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#">blog</a>
+                        <ul>
+                            <li>
+                                <a href="#">blog 1</a>
+                                <ul>
+                                    <li><a href="blog-grid-2.html">blog 2</a></li>
+                                    <li><a href="blog-grid-3.html">blog 3</a></li>
+                                    <li><a href="blog-grid-4.html">blog 4</a></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="#">blog 2</a>
+                                <ul>
+                                    <li><a href="blog2-grid-2.html">blog 2</a></li>
+                                    <li><a href="blog2-grid-3.html">blog 3</a></li>
+                                    <li><a href="blog2-grid-4.html">blog 4</a></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="#">blog 3</a>
+                                <ul>
+                                    <li><a href="blog3-grid-2.html">blog 2</a></li>
+                                    <li><a href="blog3-grid-3.html">blog 3</a></li>
+                                    <li><a href="blog3-grid-4.html">blog 4</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="blog-grid-with-sidebar.html">blog grid</a></li>
+                            <li><a href="blog-large-with-sidebar.html">blog large</a></li>
+                            <li><a href="blog-listing-with-sidebar.html">blog listing</a></li>
+                            <li><a href="blog-detail.html">blog detail</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#">team</a>
+                        <ul>
+                            <li><a href="team-overview.html">team overview</a></li>
+                            <li><a href="team-roster.html">team roster</a></li>
+                            <li><a href="team-schedule.html">team schedule</a></li>
+                            <li><a href="team-standing.html">team standing</a></li>
+                            <li><a href="team-comparison.html">team comparison</a></li>
+                            <li><a href="teamdetails.html">team details</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#">player</a>
+                        <ul>
+                            <li><a href="player-detail.html">player detail</a></li>
+                            <li><a href="players-standing.html">players standing</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#">pages</a>
+                        <ul>
+                            <li><a href="ticket.html">ticket</a></li>
+                            <li><a href="shop.html">shop</a></li>
+                            <li><a href="ticket-detail.html">ticket detail</a></li>
+                            <li><a href="404.html">404</a></li>
+                            <li><a href="comingsoon.html">comingsoon</a></li>
+                            <li><a href="widget.html">widget</a></li>
+                            <li><a href="contactus.html">contact us</a></li>
+                        </ul>
+                    </li>
+                    <li class="kf_megamenu">
+                        <a href="#">mega menu</a>
+                        <div class="megamenu_dec container">
+                            <div class="col-md-3">
+                                <h5 class="megamenu_hd">ShortCodes 1</h5>
+                                <ul class="megamenu_list">
+                                    <li><a href="ticket.html">ticket</a></li>
+                                    <li><a href="shop.html">shop</a></li>
+                                    <li><a href="ticket-detail.html">ticket detail</a></li>
+                                    <li><a href="404.html">404</a></li>
+                                    <li><a href="comingsoon.html">comingsoon</a></li>
+                                    <li><a href="widget.html">widget</a></li>
+                                    <li><a href="contactus.html">contact us</a></li>
+                                </ul>
+                            </div>
+                            <div class="col-md-3">
+                                <h5 class="megamenu_hd">ShortCodes 1</h5>
+                                <ul class="megamenu_list">
+                                    <li><a href="ticket.html">ticket</a></li>
+                                    <li><a href="shop.html">shop</a></li>
+                                    <li><a href="ticket-detail.html">ticket detail</a></li>
+                                    <li><a href="404.html">404</a></li>
+                                    <li><a href="comingsoon.html">comingsoon</a></li>
+                                    <li><a href="widget.html">widget</a></li>
+                                    <li><a href="contactus.html">contact us</a></li>
+                                </ul>
+                            </div>
+                            <div class="col-md-3">
+                                <h5 class="megamenu_hd">ShortCodes 1</h5>
+                                <ul class="megamenu_list">
+                                    <li><a href="ticket.html">ticket</a></li>
+                                    <li><a href="shop.html">shop</a></li>
+                                    <li><a href="ticket-detail.html">ticket detail</a></li>
+                                    <li><a href="404.html">404</a></li>
+                                    <li><a href="comingsoon.html">comingsoon</a></li>
+                                    <li><a href="widget.html">widget</a></li>
+                                    <li><a href="contactus.html">contact us</a></li>
+                                </ul>
+                            </div>
+                            <div class="col-md-3">
+                                <h5 class="megamenu_hd">ShortCodes 1</h5>
+                                <ul class="kf_table">
+                                    <li>
+                                        <div class="table_no">
+                                            <span>1</span>
+                                        </div>
+                                        <div class="team_logo">
+                                            <span><img src="images/team_logo.png" alt=""></span>
+                                            <a href="#">Ac milan</a>
+                                        </div>
+                                        <div class="team_point">
+                                            <span>99</span>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div class="table_no">
+                                            <span>2</span>
+                                        </div>
+                                        <div class="team_logo">
+                                            <span><img src="images/team_logo2.png" alt=""></span>
+                                            <a href="#">Chelsae</a>
+                                        </div>
+                                        <div class="team_point">
+                                            <span>94</span>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div class="table_no">
+                                            <span>3</span>
+                                        </div>
+                                        <div class="team_logo">
+                                            <span><img src="images/team_logo3.png" alt=""></span>
+                                            <a href="#">Real Madrid</a>
+                                        </div>
+                                        <div class="team_point">
+                                            <span>90</span>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div class="table_no">
+                                            <span>4</span>
+                                        </div>
+                                        <div class="team_logo">
+                                            <span><img src="images/team_logo4.png" alt=""></span>
+                                            <a href="#">Bryan munich</a>
+                                        </div>
+                                        <div class="team_point">
+                                            <span>88</span>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+                <!--DL Menu Start-->
+                <div id="kode-responsive-navigation" class="dl-menuwrapper">
+                    <button class="dl-trigger">Open Menu</button>
+                    <ul class="dl-menu">
+                        <li><a href="index-2.html">home</a></li>
+                        <li class="menu-item kode-parent-menu">
+                            <a href="#">Fixtures & Results</a>
+                            <ul class="dl-submenu">
+                                <li><a href="latest-result.html">latest result</a></li>
+                                <li><a href="team-schedule.html">teamschedule</a></li>
+                            </ul>
+                        </li>
+                        <li class="menu-item kode-parent-menu"><a href="#">blog</a>
+                            <ul class="dl-submenu">
+                                <li>
+                                    <a href="#">blog 1</a>
+                                    <ul class="dl-submenu">
+                                        <li><a href="blog-grid-2.html">blog 2</a></li>
+                                        <li><a href="blog-grid-3.html">blog 3</a></li>
+                                        <li><a href="blog-grid-4.html">blog 4</a></li>
+                                    </ul>
+                                </li>
+                                <li>
+                                    <a href="#">blog 2</a>
+                                    <ul class="dl-submenu">
+                                        <li><a href="blog2-grid-2.html">blog 2</a></li>
+                                        <li><a href="blog2-grid-3.html">blog 3</a></li>
+                                        <li><a href="blog2-grid-4.html">blog 4</a></li>
+                                    </ul>
+                                </li>
+                                <li>
+                                    <a href="#">blog 3</a>
+                                    <ul class="dl-submenu">
+                                        <li><a href="blog3-grid-2.html">blog 2</a></li>
+                                        <li><a href="blog3-grid-3.html">blog 3</a></li>
+                                        <li><a href="blog3-grid-4.html">blog 4</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="blog-grid-with-sidebar.html">blog grid</a></li>
+                                <li><a href="blog-large-with-sidebar.html">blog large</a></li>
+                                <li><a href="blog-listing-with-sidebar.html">blog listing</a></li>
+                                <li><a href="blog-detail.html">blog detail</a></li>
+                            </ul>
+                        </li>
+                        <li class="menu-item kode-parent-menu"><a href="#">team</a>
+                            <ul class="dl-submenu">
+                                <li><a href="team-overview.html">team overview</a></li>
+                                <li><a href="team-roster.html">team roster</a></li>
+                                <li><a href="team-schedule.html">team schedule</a></li>
+                                <li><a href="team-standing.html">team standing</a></li>
+                                <li><a href="team-comparison.html">team comparison</a></li>
+                                <li><a href="teamdetails.html">team details</a></li>
+                            </ul>
+                        </li>
+                        <li class="menu-item kode-parent-menu"><a href="#">player</a>
+                            <ul class="dl-submenu">
+                                <li><a href="player-detail.html">player detail</a></li>
+                                <li><a href="players-standing.html">players standing</a></li>
+                            </ul>
+                        </li>
+                        <li class="menu-item kode-parent-menu"><a href="#">pages</a>
+                            <ul class="dl-submenu">
+                                <li><a href="ticket.html">ticket</a></li>
+                                <li><a href="shop.html">shop</a></li>
+                                <li><a href="ticket-detail.html">ticket detail</a></li>
+                                <li><a href="404.html">404</a></li>
+                                <li><a href="comingsoon.html">comingsoon</a></li>
+                                <li><a href="widget.html">widget</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="contactus.html">contact us</a></li>
+                    </ul>
+                </div>
+                <!--DL Menu END-->
+                <!--Navigation End-->
+                <a href="#" class="kf_cart">
+                    <i class="fa fa-shopping-basket "></i>
+                    <div class="text">
+                        <span>Your Cart (03)</span>
+                        <em>$1200.00</em>
+                    </div>
+                </a>
+            </div>
+            <!--Navigation Wrap End-->
+        </div>
+        <!--Logo Bar End-->
+    </div>
+</header>
---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- Mirrored from kodeforest.net/html/gameplay/ by HTTrack Website Copier/3.x [XR&CO'2014], Sun, 19 Jul 2020 10:28:10 GMT -->
+<!-- Added by HTTrack --><meta http-equiv="content-type" content="text/html;charset=UTF-8" /><!-- /Added by HTTrack -->
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Home - GamePlay Sports HTML Theme</title>
+
+{{ Html::style(url('css/bootstrap.css')) }}
+{{ Html::style(url('js/dl-menu/component.css')) }}
+{{ Html::style(url('css/slick.css')) }}
+{{ Html::style(url('css/slick-theme.css')) }}
+{{ Html::style(url('css/font-awesome.css')) }}
+{{ Html::style(url("css/svg-icons.css")) }}
+{{ Html::style(url("css/prettyPhoto.css")) }}
+{{ Html::style(url("css/typography.css")) }}
+{{ Html::style(url("css/widget.css")) }}
+{{ Html::style(url("css/shortcodes.css")) }}
+{{ Html::style(url("css/style.css")) }}
+{{ Html::style(url("css/color.css")) }}
+{{ Html::style(url("css/responsive.css")) }}
+
+</head>
+
+<body>
+
+<!--kode Wrapper Start-->
+<div class="kode_wrapper">
+    <!--Header 2 Wrap Start-->
+    @include('includes.components.header')
+
+    <!--Header 2 Wrap End-->
+      @yield('content')
+
+
+    <!--Footer Wrap Start-->
+    @include('includes.components.footer')
+    <!--Footer Wrap End-->
+    <!--Copy Right Wrap Start-->
+    <div class="copy_right3">
+        <div class="container">
+            <p>2016 @ Gameplay  Design with love <a href="#">KODEFOREST</a></p>
+            <ul class="kf_social3">
+                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                <li><a href="#"><i class="fa fa-rss"></i></a></li>
+            </ul>
+        </div>
+    </div>
+    <!--Copy Right Wrap End-->
+    <!--Register Pop Up Wrap Start-->
+    <div class="modal fade" id="search" tabindex="-1" role="dialog" aria-labelledby="search">
+        <div class="modal-dialog" role="document">
+            <div class="input_dec">
+                <input type="text" placeholder="search......">
+                <button class="btn_icon"><i class="fa fa-search"></i></button>
+            </div>
+        </div>
+    </div>
+    <!--Register Pop Up Wrap End-->
+</div>
+<!--kode Wrapper End-->
+<!--Jquery Library-->
+<script src="js/jquery.js"></script>
+<!--Bootstrap core JavaScript-->
+<script src="js/bootstrap.js"></script>
+<!--Slick Slider JavaScript-->
+<script src="js/slick.min.js"></script>
+<!--Number Count (Waypoints) JavaScript-->
+<script src="js/downCount.js"></script>
+<script src="js/waypoints-min.js"></script>
+<!--Dl Menu Script-->
+<script src="js/dl-menu/modernizr.custom.js"></script>
+<script src="js/dl-menu/jquery.dlmenu.js"></script>
+<!--Progress bar JavaScript-->
+<script src="js/jprogress.js" type="text/javascript"></script>
+<!--Pretty Photo JavaScript-->
+<script src="js/jquery.prettyPhoto.js"></script>
+<!--Custom JavaScript-->
+<script src="js/custom.js"></script>
+
+</body>
+
+<!-- Mirrored from kodeforest.net/html/gameplay/ by HTTrack Website Copier/3.x [XR&CO'2014], Sun, 19 Jul 2020 10:31:10 GMT -->
+</html>
